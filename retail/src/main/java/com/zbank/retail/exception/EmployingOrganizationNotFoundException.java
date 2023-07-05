1083c9eca8ef86457cd9a2aa82f18f4cda4290de
Subhasis <subhasis.patra@hotmail.com>
2023-07-05 21:37:38 +0530

    minor change

--- retail/src/main/java/com/zbank/retail/exception/EmployingOrganizationNotFoundException.java
+++ retail/src/main/java/com/zbank/retail/exception/EmployingOrganizationNotFoundException.java
@@ -3,6 +3,6 @@ package com.zbank.retail.exception;
 public class EmployingOrganizationNotFoundException extends RuntimeException {
 
 	public EmployingOrganizationNotFoundException(String orgCode) {
-		super("Could not find employing organization " + orgCode);
+		super("Could not find employing organization : " + orgCode);
 	}
 }
